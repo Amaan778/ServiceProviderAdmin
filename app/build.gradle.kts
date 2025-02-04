38f218a7f1102d94f83936e10197b9f4b3497859
Amaan Ansari <amaanansari778@gmail.com>
2025-02-04 06:34:38 +0530

    Service Provider admin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.app.serviceprovideradmin"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.app.serviceprovideradmin"
@@ -45,4 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation ("com.intuit.sdp:sdp-android:1.1.0")
+    implementation ("com.github.yalantis:ucrop:2.2.6")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation ("com.github.denzcoskun:ImageSlideshow:0.1.2")
 }
--- app/src/main/java/com/app/serviceprovideradmin/MainActivity.kt
+++ app/src/main/java/com/app/serviceprovideradmin/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.app.serviceprovideradmin
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +13,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,LoginActivity::class.java))
+            finish()
+        },3000)
+
     }
 }
