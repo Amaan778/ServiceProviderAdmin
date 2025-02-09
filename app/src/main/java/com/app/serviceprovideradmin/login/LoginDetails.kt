9fb3447d46be28145ac6ffc595ea9f23c50dc4d9
Amaan Ansari <amaanansari778@gmail.com>
2025-02-09 23:12:31 +0530

    ServiceProvider Admin App

---
+++ app/src/main/java/com/app/serviceprovideradmin/login/LoginDetails.kt
@@ -0,0 +1,17 @@
+package com.app.serviceprovideradmin.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.app.serviceprovideradmin.R
+
+class LoginDetails : AppCompatActivity() {
+    
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_details)
+    }
+}
